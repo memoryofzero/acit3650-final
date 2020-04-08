c4c99cdbcda8c197fde2e21596090d732c35e247
zero <aqx.peng@gmail.com>
2020-04-08 10:22:48 -0700

    Initial Framework

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Home from './view/Home';
+import Map from './view/Map';
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+    },
+    Map: {
+      screen: Map,
+    },
+  },
+  {
+    initalRouteName: 'Map',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <AppContainer/>
+    </PaperProvider>
+  );
+}
+
